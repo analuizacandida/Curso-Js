40b0819f525771838943d5e6e582462fc9ed06d3
Ana Luiza Carvalho <analuizacandidavc@gmail.com>
2023-09-18 10:31:50 -0300

    commit

---
+++ aula07/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log (`A velocidade do seu carro é ${vel}KM/h`)
+if (vel >= 60) {
+        console.log(`Você ultrapasssou a velocidade permitida. Multado`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
